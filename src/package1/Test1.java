49d2b065e6d3364e81f442e3dff25828e44738ac
Anjuma Mohammad <anjuma.mohammad01@gmail.com>
2022-02-08 07:53:12 +0530

    first commit

---
+++ src/package1/Test1.java
@@ -0,0 +1,37 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Test1
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		//DesiredCapabilities -->Class
+				DesiredCapabilities cap= DesiredCapabilities.chrome();
+				cap.acceptInsecureCerts();
+				cap.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+				cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+				
+				//ChromeOptions --> Class
+				ChromeOptions options=new ChromeOptions();
+				options.merge(cap);
+				
+				System.setProperty("webdriver.chrome.driver", "./Drivers//chromedriver.exe");
+				
+				WebDriver driver= new ChromeDriver(options);
+				
+				driver.manage().window().maximize();
+				driver.get("https://expired.badssl.com/");
+				
+				//driver.quit();
+
+
+	}
+
+}
