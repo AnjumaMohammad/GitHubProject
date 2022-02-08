73c1d9f4bea0eb59e592f4252a51c68624c4b024
Anjuma2 <anjuma2@test.com>
2022-02-08 10:16:02 +0530

    anjuma2 is commiting this change

--- src/package1/Test1.java
+++ src/package1/Test1.java
@@ -6,31 +6,30 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class Test1
-{
+public class Test1 {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		//DesiredCapabilities -->Class
-				DesiredCapabilities cap= DesiredCapabilities.chrome();
-				cap.acceptInsecureCerts();
-				cap.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-				cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-				
-				//ChromeOptions --> Class
-				ChromeOptions options=new ChromeOptions();
-				options.merge(cap);
-				
-				System.setProperty("webdriver.chrome.driver", "./Drivers//chromedriver.exe");
-				
-				WebDriver driver= new ChromeDriver(options);
-				
-				driver.manage().window().maximize();
-				driver.get("https://expired.badssl.com/");
-				
-				//driver.quit();
+		// DesiredCapabilities -->Class
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		cap.acceptInsecureCerts();
+		cap.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 
+		// ChromeOptions --> Class
+		ChromeOptions options = new ChromeOptions();
+		options.merge(cap);
+
+		System.setProperty("webdriver.chrome.driver", "./Drivers//chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.manage().window().maximize();
+		driver.get("https://expired.badssl.com/");
+		Thread.sleep(2000);
+		System.out.println("user2 has updated this code");
+
+		driver.quit();
 
 	}
 
